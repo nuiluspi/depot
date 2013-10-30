fba459a1cb1d56c907cee3896b7dfc556cce3bbf
Lukas <Lukas@photoqt.org>
2013-10-30 12:05:11 +0000

    attempt to highlight current item (not (yet) working)

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -78,7 +78,7 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to(store_url, :notice => 'Your cart is currently empty') }
+      format.html { redirect_to(store_url, :notice => 'Your shopping cart is empty') }
       format.json { head :no_content }
     end
   end
--- app/views/line_items/create.js.builder
+++ app/views/line_items/create.js.builder
@@ -1,5 +1,5 @@
 $('#cart').html '<%= j render(@cart) %>'
 
-$[:current_item].visual_effect :highlight, 
+$('#current_item').visual_effect :highlight, 
                                   :start_color => "#88ff88",
                                   :endcolor => "#114411"
